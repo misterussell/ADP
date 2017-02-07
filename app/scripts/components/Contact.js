3edb1b429f007f1196d3376318ad163631ffb586
Maxwell Russell <maxruss.87@gmail.com>
2017-02-07 15:41:05 -0600

    uploading formspree content

---
+++ app/scripts/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <form
+        action="https://formspree.io/max@misterussell.com"
+        method="POST">
+        <input type="text" name="name">
+        <input type="email" name="_replyto">
+        <input type="submit" value="Send">
+      </form>
+    )
+  }
+})
