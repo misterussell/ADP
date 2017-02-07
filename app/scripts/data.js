348a6831af7aac669f1dd02e38cf62581e907598
Maxwell Russell <maxruss.87@gmail.com>
2017-02-06 20:45:53 -0600

    updating staff data array

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,5 +1,74 @@
 const pathologists = [
-  {},
+  {
+    name: 'Dr. Alfred W. Campbell',
+    certifications: 'Anatomic and Clinical Pathology',
+    degree: 'Texas A&M University, Biology',
+    school: 'University of Texas at Houston',
+    training: 'University of Texas Southwestern in Dallas',
+    experience: 'Group practice in Virginia and the Carolinas',
+    rolesAndAttr: 'Medical Director, Group President',
+    associations: 'Past President of Virginia Society for Pathology, Leadership roles in American Medical Association and College of American Pathologists, Founding Partner of American Diagnostic Pathology'
+  },
+  {
+    name: 'Dr. John A. Caldwell',
+    certifications: 'Anatomic Pathology and Clinical Pathology, Dermatopathology',
+    degree: 'Austin College, Sherman, Texas',
+    school: 'University of Texas at Houston',
+    training: 'University of Texas Health Science Center Houstan Texas, Subspecialty Dermatopathology at the University of Texas Southwestern Medical Center Dallas',
+    experience: 'Group practice in the Carolinas',
+    rolesAndAttr: 'Medical Director, Department chair, Founding Partner of American Diagnostic Pathology',
+    associations: null
+  },
+  {
+    name: 'Dr. Rosanna L. Lapham',
+    certifications: 'Anatomic and Clinical Pathology, Fellow of the Royal College of Pathologist',
+    degree: 'Southwestern University, Georgetown, Texas',
+    school: 'University of Texas, Galveston Texas',
+    training: 'University of Dundee Ninewells Hospital, Dundee Scotland, University of Texas at Houston,M. D. Anderson Cancer Center',
+    experience: 'New Zealand, group practice in the Carolinas',
+    rolesAndAttr: 'Histology technical supervisor, Pathology assistant supervisor, quality assurance, Founding Associate Partner American Diagnostic Pathology',
+    associations: 'College of American Pathologists, Royal College of Pathologists'
+  },
+  {
+    name: 'Dr. Tarek M. Bishara',
+    certifications: 'Anatomic and Clinical Pathology',
+    degree: 'Francis Marion University',
+    school: 'University of South Carolina School of Medicine',
+    training: 'University of Texas Southwestern in Dallas',
+    experience: 'Numerous years in private practice in Lexington, Kentucky and Florence, SC, Pee Dee Pathology Associates',
+    rolesAndAttr: 'Subspecialty training in Urologic Pathology, Medical Director of Molecular Diagnostics Laboratory, Practice Management',
+    associations: 'College of American Pathologists, International Society of Urologic Pathology, Founding Associate Partner American Diagnostic Pathology'
+  },
+  {
+    name: 'Dr. Jared G. Block',
+    certifications: 'Anatomic and Clinical Pathology, Hematopathology',
+    degree: 'Emory University in Atlanta, Georgia',
+    school: 'University of Illinois Medical School',
+    training: 'University of Missouri; Hematopathology Fellowship Charlotte NC',
+    experience: 'Group practice in the Carolinas',
+    rolesAndAttr: 'Hematopathology, coagulation disorders',
+    associations: 'College of American Pathologists, Founding Associate Partner American Diagnostic Pathology'
+  },
+  {
+    name: 'Dr. R. Tucker Burks',
+    certifications: 'Anatomic and Clinical Pathology',
+    degree: 'Davidson College, Davidson NC',
+    school: 'Medical University of South Carolina',
+    training: 'George Washington University Medical Center including fellowship training molecular diagnostics; gynecologic pathology Johns Hopkins University',
+    experience: 'Faculty at Medical College of Virginia in Richmond, group practice in the Carolinas',
+    rolesAndAttr: 'Gynecologic pathology, Business development, Founding Associate Member American Diagnostic Pathology',
+    associations: 'International Society of Gynecologic Pathologists and International Society of Urologic Pathology'
+  },
+  {
+    name: 'Dr. E. Travis Smith',
+    certifications: 'Anatomic and Clinical Pathology, Hematopathology',
+    degree: 'University of North Carolina Chapel Hill',
+    school: 'University of North Carolina School of Medicine',
+    training: 'Bowman Gray School of Medicine, Winston-Salem NC, Hematopathology subspecialty training',
+    experience: 'Group Practices in Virginia and the Carolinas',
+    rolesAndAttr: 'Hematopathology, gastrointestinal pathology',
+    associations: 'College of American Pathologists, Founding Associate Partner American Diagnostic Pathology'
+  },
 ];
 
 export default pathologists;
